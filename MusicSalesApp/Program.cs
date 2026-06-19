91425834afc4c5414031d5051d9e7bc63a0d2a4e
David Warwick <bgmsd@live.com>
2026-06-18 19:16:42 -0700

    Fix learnmore route ambiguity

--- MusicSalesApp/Program.cs
+++ MusicSalesApp/Program.cs
@@ -510,7 +510,6 @@ try
     app.MapStaticAssets();
     app.MapControllers();
     app.MapRazorPages(); // Add Razor Pages routing
-    app.MapGet("/LearnMore", () => Results.Redirect("/learnmore", permanent: true));
     app.MapRazorComponents<App>()
         .AddInteractiveServerRenderMode();
 
